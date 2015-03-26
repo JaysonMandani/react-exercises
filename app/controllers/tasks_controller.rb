3920826ef433700fc8c45ae42abcf3e803b8e361
Jayson Mandani <jaysonmandani@gmail.com>
2015-03-26 09:38:05 +0800

    Adds model and controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,22 @@
 class TasksController < ApplicationController
   def index
-    
+    @tasks = Task.all
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
 
+    if request.xhr?
+      render :json => @task
+    else
+      redirect_to tasks_path
+    end
   end
 
   private
 
   def task_params
-    
+    require.params(:task).permit(:name)
   end
 end
